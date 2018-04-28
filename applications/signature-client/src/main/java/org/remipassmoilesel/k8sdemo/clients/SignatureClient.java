9ff75e0020c893aa48e0d78cd54bd3fe80d4193c
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-28 16:52:39 +0200

    Add signature service client

---
+++ applications/signature-client/src/main/java/org/remipassmoilesel/k8sdemo/clients/SignatureClient.java
@@ -0,0 +1,39 @@
+package org.remipassmoilesel.k8sdemo.clients;
+
+import io.reactivex.Single;
+import org.remipassmoilesel.k8sdemo.commons.AbstractSyncClient;
+import org.remipassmoilesel.k8sdemo.commons.comm.MCMessage;
+import org.remipassmoilesel.k8sdemo.commons.comm.sync.MicroCommSync;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SignatureClient extends AbstractSyncClient {
+
+    public SignatureClient(MicroCommSync microCommSync) {
+        super(microCommSync);
+    }
+
+    public Single<List<SignedDocument>> getDocuments() {
+        return microCommSync.request("getDocuments", MCMessage.EMPTY)
+                .map((message) -> {
+                    return (List<SignedDocument>) message.getContent()[0];
+                });
+    }
+
+    public Single<SignedDocument> persistAndSignDocument(SignedDocument document) {
+        return microCommSync.request("persistAndSignDocument", MCMessage.fromObject(document))
+                .map(message -> (SignedDocument) message.getContent()[0]);
+    }
+
+    // TODO: return Single<Void> or better type
+    public Single<MCMessage> deleteDocument(String documentId) {
+        return microCommSync.request("deleteDocument", MCMessage.fromObject(documentId));
+    }
+
+    public Single<GpgValidationResult> checkDocument(SignedDocument document) {
+        return microCommSync.request("checkDocument", MCMessage.fromObject(document))
+                .map(message -> (GpgValidationResult) message.getContent()[0]);
+    }
+}
