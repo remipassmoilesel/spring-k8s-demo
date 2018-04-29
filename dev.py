1d0065e583d0b6725ba6bbf05fae430a818826f8
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-04-30 00:05:11 +0200

    Move python helpers to a separated dir

--- dev.py
+++ dev.py
@@ -2,48 +2,8 @@
 
 import subprocess
 import sys
-
-
-class TermStyle():
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    CYAN = "\033[1;36m"
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
-
-
-class Utils:
-    @staticmethod
-    def runCommand(command):
-        subprocess.run(command, shell=True, check=True)
-
-    @staticmethod
-    def log(message='', data=None, termStyle=TermStyle.GREEN):
-        print(Utils.colorize(message, termStyle=termStyle))
-        if data:
-            print(data)
-
-    @staticmethod
-    def colorize(message, termStyle=TermStyle.GREEN):
-        return termStyle + str(message) + TermStyle.RESET
-
-
-class ActionHandlers():
-
-    def showHelp(self):
-        Utils.log('Control application in development environment')
-        Utils.log('')
-        Utils.log('Examples: ')
-        Utils.log('\t$ dev start')
-        Utils.log('\t$ dev stop')
-
-    def exit(self, code=0):
-        exit(code)
-
+from scripts import ActionHandlers
+from scripts import Utils
 
 class ArgParser:
     def __init__(self):
---
+++ scripts/__init__.py
@@ -0,0 +1,3 @@
+from .utils import TermStyle
+from .utils import Utils
+from .utils import ActionHandlers
