263d96b55fbc48eef90eb6a85bc40c57fba25ef4
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-05-01 11:27:10 +0200

    Remove useless values

--- applications/nats-helm-chart/ck-config.js
+++ applications/nats-helm-chart/ck-config.js
@@ -1,42 +1,12 @@
 
 module.exports = {
-  "name": "application-name",
-  "displayCommandsOutput": true,
-  "applicationStructure": "deployment",
-  "defaultEnvironment": "dev",
+  "name": "nats-broker",
+  "applicationStructure": "helm-chart",
+  "defaultEnvironment": "test",
   "scripts": {
-    "buildDev": "./build --fancy application",
-    "runDev": "./run --without-bug application",
     "helmDebug": "helm install --dry-run --debug .",
-    "kubectlDebug": "kubectl create -f . --dry-run"
-  },
-  "docker": {
-    "imageName": "deployment-with-docker-file",
-    "tag": "0.1",
-    "push": true,
-    "buildDirectory": "./path/to/docker/build"
-  },
-  "deployment": {
-    "roots": [
-      ".",
-      "./second/dir"
-    ]
   },
   "helm": {
-    "releaseName": "gitlab-dev"
+    "releaseName": "test-nats"
   },
-  "ansible": {
-    "inventoryDirectory": "./path/to/dir/",
-    "playbooks": {
-      "deploy": "#/scripts/kubespray/cluster.yml",
-      "destroy": "#/scripts/kubespray/reset.yml"
-    }
-  },
-  "hooks": {
-    "preBuild": "./pre-build.sh",
-    "preDeploy": "./pre-deploy.sh",
-    "postDeploy": "./post-deploy.sh",
-    "preDestroy": "./pre-destroy.sh",
-    "postDestroy": "./post-destroy.sh"
-  }
 }
